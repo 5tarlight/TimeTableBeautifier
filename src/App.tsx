76b62945ac455a8568a2ae151d712b896b47b8f6
YEAHx4 <developerstarlight@gmail.com>
2024-03-07 22:37:35 +0900

    dynamically render time table content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
 import "./App.css";
-import { days, getTimes } from "./lib";
+import { TimeTable, days, getCellProps, getTimes, toTime } from "./lib";
 
 function App() {
+  const [data, setData] = useState<TimeTable>([
+    [
+      {
+        color: "red",
+        end: 11,
+        start: 7,
+        location: "공학관 407",
+        name: "웹프로그래밍",
+        professor: "김교수",
+      },
+    ],
+    [
+      {
+        color: "yellowgreen",
+        start: 4,
+        end: 8,
+        name: "운영체제",
+        professor: "박교수",
+        location: "공학관 301",
+      },
+      {
+        color: "lightcoral",
+        start: 10,
+        end: 14,
+        name: "컴퓨터구조",
+        professor: "이교수",
+        location: "공학관 401",
+      },
+    ],
+    [],
+    [
+      {
+        color: "skyblue",
+        start: 1,
+        end: 6,
+        name: "데이터베이스",
+        professor: "최교수",
+        location: "창의관 304",
+      },
+    ],
+    [],
+  ]);
+
   return (
     <>
       <header>
@@ -12,6 +56,7 @@ function App() {
         <table>
           <thead>
             <tr>
+              <th></th>
               {days.map((day, index) => (
                 <th key={index}>{day}</th>
               ))}
@@ -21,11 +66,18 @@ function App() {
             {getTimes().map((time, index) => {
               return (
                 <tr key={index}>
-                  <th>{time}</th>
-                  {[0, 1, 2, 3, 4].map((i, index) => {
+                  <th>{toTime(time)}</th>
+                  {[0, 1, 2, 3, 4].map((date, index) => {
+                    const props = getCellProps(date, time, data);
+
                     return (
-                      <td key={index}>
-                        {time}-{i}
+                      <td
+                        key={index}
+                        style={{
+                          backgroundColor: props.color,
+                        }}
+                      >
+                        {props.text}
                       </td>
                     );
                   })}
--- src/lib.ts
+++ src/lib.ts
@@ -1,12 +1,89 @@
-export const days = ["", "월", "화", "수", "목", "금"];
+export const days = ["월", "화", "수", "목", "금"];
+
+// export const getTimes = () => {
+//   const times = [];
+
+//   for (let i = 9; i <= 18; i++) {
+//     times.push(`${i}:00`);
+//     times.push(`${i}:30`);
+//   }
+
+//   return times;
+// };
 
 export const getTimes = () => {
-  const times = [];
+  return Array.from({ length: 21 }, (_, index) => index);
+};
 
-  for (let i = 9; i <= 18; i++) {
-    times.push(`${i}:00`);
-    times.push(`${i}:30`);
+export interface Lecture {
+  name: string;
+  professor: string;
+  location: string;
+  start: number;
+  end: number;
+  color: string;
+}
+
+export type TimeTable = [Lecture[], Lecture[], Lecture[], Lecture[], Lecture[]];
+
+export const toTime = (time: number) => {
+  return `${Math.floor(time / 2) + 9}:${time % 2 === 0 ? "00" : "30"}`;
+};
+
+export const findLecture = (date: number, time: number, data: TimeTable) => {
+  for (let i = 0; i < data[date].length; i++) {
+    if (data[date][i].start <= time && data[date][i].end > time) {
+      return data[date][i];
+    }
   }
 
-  return times;
+  return null;
+};
+
+// export const getCurrentColor = (
+//   date: number,
+//   time: number,
+//   data: TimeTable
+// ) => {
+//   for (let i = 0; i < data[date].length; i++) {
+//     if (data[date][i].start <= time && data[date][i].end > time) {
+//       return data[date][i].color;
+//     }
+//   }
+
+//   return "transparent";
+// };
+
+export interface TimeCellProps {
+  color: string;
+  isStart: boolean;
+  isEnd: boolean;
+  line: number;
+  text: string;
+}
+
+export const getCellProps = (date: number, time: number, data: TimeTable) => {
+  const lecture = findLecture(date, time, data);
+
+  if (!lecture) {
+    return {
+      color: "transparent",
+      isStart: false,
+      isEnd: false,
+      line: time,
+      text: "",
+    };
+  }
+
+  const color = lecture.color;
+  const isStart = lecture.start === time;
+  const isEnd = lecture.end === time;
+
+  return {
+    color,
+    isStart,
+    isEnd,
+    line: time,
+    text: `${time}-${date}`,
+  };
 };
